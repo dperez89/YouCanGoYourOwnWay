37a2d54f9cb66fe9c18dc8e426ddc6d2a5f49a04
David Perez <d.perez89@aol.com>
2017-11-16 11:39:38 -0600

    adding in the beginning method of the MainStory class. The player is presented with the context of the story of their adventure.

--- ChooseYourOwnAdventureStory/ChooseYourOwnAdventureStory/MainStory.cs
+++ ChooseYourOwnAdventureStory/ChooseYourOwnAdventureStory/MainStory.cs
@@ -23,29 +23,46 @@ namespace ChooseYourOwnAdventureStory
             Console.WriteLine("In this game, you will choose your actions that will effect the potential outcome of the game.");
             Console.WriteLine("Ultimately you want to try and avoid dying, but don't let the fear of death dissuade you from the thrill of adventure!");
             Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("So.. do you want to play? You can either enter the number of the selection you'd like or enter the KEYWORD of what you'd like to do.");
+            Console.WriteLine("So.. do you want to play? You can enter the KEYWORD of what you'd like to do.");
             Console.WriteLine(Environment.NewLine);
             Console.WriteLine("START Game", Environment.NewLine);
             Console.WriteLine("QUIT Game");
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("What will you do?:");
             userInput = Console.ReadLine().ToLower();
 
             if (userInput == "start")
             {
-                Console.WriteLine("You have started a new game!");
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("Very well then, let us begin your story!");
+                BeginGame();
             }
             else if (userInput == "quit")
             {
-                Console.WriteLine("You have quit the game!");
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("Hope to see you again soon!");
             }
             else
             {
+                Console.WriteLine(Environment.NewLine);
                 Console.WriteLine("Command not recognized, please try again");
+                Console.ReadKey();
                 Console.Clear();
                 MainMenu();
             }
         }
         public void BeginGame()
         {
+            Console.Clear();
+            Console.WriteLine("The Story...");
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("You are taking the role of an adventurer who has just entered into a new land. A true frontier of mysteries, perils, and wonders.");
+            Console.WriteLine("However, your journey here is not one of leisure. You have been tasked with traveling to the far side of a dark forest that lay before you.");
+            Console.WriteLine("A village at the far edge of the forest eagerly awaits your arrival so that you might lend a hand in protecting them from an invading force");
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("You know that navigating this forest will be a test of your character, but you are determined all the same.");
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("You slowly take your first steps across the threshold of the forest...");
 
         }
         public void EndGame()
